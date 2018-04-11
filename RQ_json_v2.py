7e17e543c88bb997d5474ba725b9230c2e77df3c
Juan Fernando Márquez Garcés <32533941+jfernandomarquez@users.noreply.github.com>
2018-04-11 17:30:30 -0500

    Update RQ_json_v2.py

--- RQ_json_v2.py
+++ RQ_json_v2.py
@@ -5,7 +5,7 @@ import json
 print "###################################  SUBO  ###############################################"
 print '\n'
 
-r = requests.post('http://10.100.64.229:9200/blog/user/json/', json={"content": "CONTENIDO"})
+r = requests.post('http://192.168.20.1:9200/blog/user/json/', json={"content": "CONTENIDO"})
 
 print r.text
 
@@ -14,12 +14,12 @@ print "\n"
 print "#################################  CONSULTO  ##############################################"
 print "\n"
 
-response = requests.get('http://10.100.64.229:9200/blog/user/json/') ###### Funciona #####
+response = requests.get('http://192.168.20.1:9200/blog/user/json/') ###### Funciona #####
 print response.text
 
 print "\n"
 print "################################  SEARCH  ################################################"
 print "\n"
 
-response2= requests.post('http://10.100.64.229:9200/blog/_search?pretty=true', json={'query': {'match': { 'content': 'CONTENIDO'}}})
+response2= requests.post('http://192.168.20.1:9200/blog/_search?pretty=true', json={'query': {'match': { 'content': 'CONTENIDO'}}})
 print response2.text
