05c186d6d9ee63f590054be8fcf93840ed84a325
Juan Fernando Márquez Garcés <32533941+jfernandomarquez@users.noreply.github.com>
2018-04-10 22:04:58 -0500

    Update py-Elastic.py

--- py-Elastic.py
+++ py-Elastic.py
@@ -1,6 +1,6 @@
 from elasticsearch import Elasticsearch
 
-es = Elasticsearch(['192.168.1.58:9200']) #esclient
+es = Elasticsearch(['192.168.20.1:9200']) #esclient
 #es=Elasticsearch([{'host': 'localhost', 'port': 9200}])
 r=es.search()
 print r
