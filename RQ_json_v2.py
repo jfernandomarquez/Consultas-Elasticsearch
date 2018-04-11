cb05679614c59570785c7d5bb821a63f58b4911f
Juan Fernando Márquez Garcés <32533941+jfernandomarquez@users.noreply.github.com>
2018-04-11 17:26:31 -0500

    Add files via upload

---
+++ RQ_json_v2.py
@@ -0,0 +1,25 @@
+import requests
+import json
+
+
+print "###################################  SUBO  ###############################################"
+print '\n'
+
+r = requests.post('http://10.100.64.229:9200/blog/user/json/', json={"content": "CONTENIDO"})
+
+print r.text
+
+
+print "\n"
+print "#################################  CONSULTO  ##############################################"
+print "\n"
+
+response = requests.get('http://10.100.64.229:9200/blog/user/json/') ###### Funciona #####
+print response.text
+
+print "\n"
+print "################################  SEARCH  ################################################"
+print "\n"
+
+response2= requests.post('http://10.100.64.229:9200/blog/_search?pretty=true', json={'query': {'match': { 'content': 'CONTENIDO'}}})
+print response2.text
