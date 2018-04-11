2185616b1f218eaf8672dfe6b8a954cffa9219b7
Juan Fernando Márquez Garcés <32533941+jfernandomarquez@users.noreply.github.com>
2018-04-10 21:53:19 -0500

    Update and rename cargarejem.sh to Cargar_ejem_doc.sh

---
+++ Cargar_ejem_doc.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+curl -H 'Content-Type: application/x-ndjson' -XPOST 'http://192.168.20.1:9200/bank/account/_bulk?pretty' --data-binary @accounts.json
+curl -H 'Content-Type: application/x-ndjson' -XPOST 'http://192.168.20.1:9200/shakespeare/doc/_bulk?pretty' --data-binary @shakespeare_6.0.json
+curl -H 'Content-Type: application/x-ndjson' -XPOST 'http://192.168.20.1:9200/_bulk?pretty' --data-binary @logs.jsonl
