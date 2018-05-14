5ed8cf0ffefeb290701c3e0fad322a542c7db41d
Juan Fernando Márquez Garcés <32533941+jfernandomarquez@users.noreply.github.com>
2018-05-14 17:11:52 -0500

    Update logstash-req.py

--- logstash-req.py
+++ logstash-req.py
@@ -1,8 +1,10 @@
+# Algoritmo para hacer consultas a los logs almacenados en elasticsearch con el indice logstash-*
+
 import requests
 import json
 
 print "\n"
-print "#################################  CONSULTO  ##############################################"
+print "#################################  CONSULTANDO LOGS ##############################################"
 print "\n"
 
 response = requests.get('http://192.168.1.60:9200/logstash-*/log/_search', json={"size":1,"query":{"match_all":{}}})) 
