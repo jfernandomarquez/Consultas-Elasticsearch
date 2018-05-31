8e439bf6d2abf45c45aa36fee20582d1ecef5555
Juan Fernando Márquez Garcés <32533941+jfernandomarquez@users.noreply.github.com>
2018-05-31 08:50:47 -0500

    Delete py-Elastic.py

--- py-Elastic.py
+++
@@ -1,9 +0,0 @@
-from elasticsearch import Elasticsearch
-
-es = Elasticsearch(['192.168.20.1:9200']) #esclient
-#es=Elasticsearch([{'host': 'localhost', 'port': 9200}])
-r=es.search()
-print r
-print "-----------------"
-print es.info()
-
