5a01bd31fef94d357211f33bf8dbe00a63d8775e
Juan Fernando Márquez Garcés <32533941+jfernandomarquez@users.noreply.github.com>
2018-05-31 08:47:44 -0500

    Delete espy_querying.py

--- espy_querying.py
+++
@@ -1,9 +0,0 @@
-from elasticsearch import Elasticsearch
- 
-es = Elasticsearch(['192.168.20.1:9200'])
-res = es.search(index="test", doc_type="articles", body={"query": {"match": {"content": "fox"}}})
-print("%d documents found" % res['hits']['total'])
-for doc in res['hits']['hits']:
-    print("%s) %s" % (doc['_id'], doc['_source']['content']))
-
-#es.create(index="test", doc_type="articles", body={"content": "One more fox"})
